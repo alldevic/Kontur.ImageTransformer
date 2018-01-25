a76888f67dd2c89e9b83d4c1721fabfe78a48610
Nikolay Bely <belyynikolay@gmail.com>
2018-01-25 14:48:42 +0700

    Add docs for entry point

--- Kontur.ImageTransformer/Program.cs
+++ Kontur.ImageTransformer/Program.cs
@@ -2,11 +2,13 @@
 
 namespace Kontur.ImageTransformer
 {
-    
     public class Program
     {
         public static bool Testcase = true;
 
+        /// <summary>
+        /// The main entry point for the application
+        /// </summary>
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
