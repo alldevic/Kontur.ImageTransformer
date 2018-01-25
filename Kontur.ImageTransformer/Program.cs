59072e7d223fc732012e61241b7f17b62d4baa4e
Nikolay Bely <belyynikolay@gmail.com>
2018-01-25 13:25:12 +0700

    Add solution template

---
+++ Kontur.ImageTransformer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kontur.ImageTransformer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
