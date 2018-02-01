b9e65291189396bf3ac95a51938df13646212d89
Nikolay Bely <belyynikolay@gmail.com>
2018-02-01 11:01:21 +0700

    Rewrite to async

--- Kontur.ImageTransformer/Controllers/BadRequestController.cs
+++ Kontur.ImageTransformer/Controllers/BadRequestController.cs
@@ -1,16 +1,16 @@
-﻿using System.Net;
-using System.Net.Http;
+﻿using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Kontur.ImageTransformer.Controllers
 {
+    /// <inheritdoc />
     /// <summary>
     /// Customize 404 NotFound error page to 400 BadRequest 
     /// </summary>
     public class BadRequestController : ApiController
     {
         [HttpPost, AcceptVerbs("PATCH")]
-        public HttpResponseMessage Handle404() =>
-            new HttpResponseMessage(HttpStatusCode.BadRequest);
+        public async Task<IHttpActionResult> Handle404() =>
+            await Task.FromResult(BadRequest());
     }
 }
