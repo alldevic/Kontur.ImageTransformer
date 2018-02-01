fc4d61fac63aef86f4e85c166ec5d0efaf2e2cf3
Nikolay Bely <belyynikolay@gmail.com>
2018-02-01 10:39:43 +0700

    Imrove and refactoring ProcessController

--- Kontur.ImageTransformer/Controllers/ProcessController.cs
+++ Kontur.ImageTransformer/Controllers/ProcessController.cs
@@ -1,21 +1,68 @@
-﻿using System.Threading.Tasks;
+﻿//#define debug
+
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Web.Http;
 using Kontur.ImageTransformer.ImageFilters;
+using Kontur.ImageTransformer.Results;
 
 namespace Kontur.ImageTransformer.Controllers
 {
+    [OverrideAuthentication, OverrideAuthorization]
     public class ProcessController : ApiController
     {
         [HttpPost]
         public async Task<IHttpActionResult> Grayscale(int x, int y, int w, int h) =>
-            await Filtering.Do(Request.Content.ReadAsStreamAsync().Result, x, y, w, h, new GrayscaleFilter());
+            await Do(x, y, w, h, new GrayscaleFilter());
 
         [HttpPost, Route("process/threshold({level:int:range(0,100)})/{x},{y},{w},{h}")]
         public async Task<IHttpActionResult> Threshold(byte level, int x, int y, int w, int h) =>
-            await Filtering.Do(Request.Content.ReadAsStreamAsync().Result, x, y, w, h, new ThresholdFilter(level));
+            await Do(x, y, w, h, new ThresholdFilter(level));
 
         [HttpPost]
         public async Task<IHttpActionResult> Sepia(int x, int y, int w, int h) =>
-            await Filtering.Do(Request.Content.ReadAsStreamAsync().Result, x, y, w, h, new SepiaFilter());
+            await Do(x, y, w, h, new SepiaFilter());
+
+        /*[HttpPost]
+        public async Task<IHttpActionResult> Crop(int x, int y, int w, int h) =>
+            await Do(x, y, w, h, new EqualFilter());*/
+
+        private async Task<IHttpActionResult> Do(int x, int y, int w, int h, IPixelFilter filter)
+        {
+            var img = new Bitmap(Request.Content.ReadAsStreamAsync().Result);
+
+            var plot = Rectangle.Intersect(new Rectangle(x, y, w, h), new Rectangle(0, 0, img.Width, img.Height));
+            if (plot.IsEmpty || plot.Width == 0 || plot.Height == 0)
+            {
+                return await Task.FromResult(new NoContentResult());
+            }
+
+            var bytes = plot.Width * plot.Height;
+            var argbValues = new int[bytes];
+
+            var bmpData = img.LockBits(plot, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+            Marshal.Copy(bmpData.Scan0, argbValues, 0, bytes);
+            img.UnlockBits(bmpData);
+
+            for (var i = 0; i < bytes; i++)
+            {
+                argbValues[i] = filter.Set(argbValues[i]);
+            }
+
+            img = new Bitmap(plot.Width, plot.Height, PixelFormat.Format32bppArgb);
+            bmpData = img.LockBits(new Rectangle(0, 0, plot.Width, plot.Height), 
+                ImageLockMode.WriteOnly, img.PixelFormat);
+            Marshal.Copy(argbValues, 0, bmpData.Scan0, bytes);
+            img.UnlockBits(bmpData);
+
+            using (var res = new MemoryStream())
+            {
+                img.Save(res, ImageFormat.Png);
+                return await Task.FromResult(new MemoryStreamResult(res));
+            }
+        }
     }
 }
