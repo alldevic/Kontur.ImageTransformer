06db33477d824d0fc0e096a98069f27ca271e207
Nikolay Bely <belyynikolay@gmail.com>
2018-02-04 05:18:55 +0700

    replace formatter via ihttpresult

--- Kontur.ImageTransformer/Controllers/ProcessController.cs
+++ Kontur.ImageTransformer/Controllers/ProcessController.cs
@@ -3,7 +3,7 @@ using System.Drawing.Imaging;
 using System.Net;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Kontur.ImageTransformer.Formatters;
+using Kontur.ImageTransformer.Results;
 
 namespace Kontur.ImageTransformer.Controllers
 {
@@ -25,30 +25,28 @@ namespace Kontur.ImageTransformer.Controllers
 
         private async Task<IHttpActionResult> Do(int x, int y, int w, int h, ImageFilters.Filter filter, int level = 0)
         {
-            if (!Request.TryToBitmap(out var img))
+            if (!Request.TryToBitmap(out var img) || img.PixelFormat != PixelFormat.Format32bppArgb ||
+                img.Width > 1000 || img.Height > 1000)
             {
-                return await Task.FromResult(BadRequest());
+                return BadRequest();
             }
 
             var plot = Rectangle.Intersect(new Rectangle(x, y, w, h), new Rectangle(0, 0, img.Width, img.Height));
             if (plot.IsEmpty || plot.Width == 0 || plot.Height == 0)
             {
-                return await Task.FromResult(StatusCode(HttpStatusCode.NoContent));
+                return StatusCode(HttpStatusCode.NoContent);
             }
 
             var bytes = plot.Width * plot.Height;
             var argbValues = img.ToArray(plot);
 
             var byteLevel = (byte) (255 * level / 100);
-            int i;
-            for (i = 0; i < bytes; i++)
+            for (var i = 0; i < bytes; i++)
             {
                 argbValues[i] = filter(argbValues[i], byteLevel);
             }
 
-            img = argbValues.ToBitmap(plot.Width, plot.Height);
-
-            return await Task.FromResult(Content(HttpStatusCode.OK, img, new BitmapWriteFormatter()));
+            return await Task.FromResult(new OkResult(img));
         }
     }
 }
---
+++ Kontur.ImageTransformer/Results/OkResult.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Kontur.ImageTransformer.Results
+{
+    /// <inheritdoc /> 
+    /// <summary> 
+    /// Write Bitmap to body of response 
+    /// </summary> 
+    public class OkResult : IHttpActionResult
+    {
+        private readonly Bitmap _value;
+        private readonly string _contetntType;
+
+        /// <summary> 
+        /// Send a content of Bitmap in body of response with specific Content-Type HTTP header 
+        /// </summary> 
+        /// <param name="value">Bitmap for pushing in reponse</param> 
+        /// <param name="contetntType">Specific Content-Type HTTP header</param> 
+        public OkResult(Bitmap value, string contetntType = "application/octet-stream")
+        {
+            _value = value;
+            _contetntType = contetntType;
+        }
+
+        public async Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage();
+            using (var ms = new MemoryStream())
+            {
+                _value.Save(ms, ImageFormat.Png);
+                response.Content = new ByteArrayContent(ms.ToArray());
+            }
+
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue(_contetntType);
+
+            return await Task.FromResult(response);
+        }
+    }
+}
