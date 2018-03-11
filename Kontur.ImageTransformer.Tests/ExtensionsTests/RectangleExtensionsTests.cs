93d6e9b158f8e94725a5d5b693787154f820947b
Nikolay Bely <belyynikolay@gmail.com>
2018-03-12 00:50:30 +0700

    Add tests for RotateFlip()

--- Kontur.ImageTransformer.Tests/ExtensionsTests/RectangleExtensionsTests.cs
+++ Kontur.ImageTransformer.Tests/ExtensionsTests/RectangleExtensionsTests.cs
@@ -34,5 +34,37 @@ namespace Kontur.ImageTransformer.Tests.ExtensionsTests
         {
             Assert.That(src.Normalise(), Is.EqualTo(res));
         }
+
+        [Test]
+        public void RotateFlip_cw_Correct()
+        {
+            var src = new Rectangle(55, 75, 70, 70);
+            var res = new Rectangle(75, 45, 70, 70);
+            Assert.That(src.RotateFlip(RotateFlipType.Rotate90FlipNone, 250, 170), Is.EqualTo(res));
+        }
+
+        [Test]
+        public void RotateFlip_ccw_Correct()
+        {
+            var src = new Rectangle(45, 105, 70, 70);
+            var res = new Rectangle(105, 55, 70, 70);
+            Assert.That(src.RotateFlip(RotateFlipType.Rotate90FlipNone, 250, 170), Is.EqualTo(res));
+        }
+
+        [Test]
+        public void RotateFlip_flipv_Correct()
+        {
+            var src = new Rectangle(105, 45, 70, 70);
+            var res = new Rectangle(45, -5, 70, 70);
+            Assert.That(src.RotateFlip(RotateFlipType.Rotate90FlipNone, 250, 170), Is.EqualTo(res));
+        }
+
+        [Test]
+        public void RotateFlip_fliph_Correct()
+        {
+            var src = new Rectangle(75, 55, 70, 70);
+            var res = new Rectangle(55, 25, 70, 70);
+            Assert.That(src.RotateFlip(RotateFlipType.Rotate90FlipNone, 250, 170), Is.EqualTo(res));
+        }
     }
 }
